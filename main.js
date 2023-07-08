c458cac87db6e6987843e88447a73c6b5579669d
max <mpavlov@mrcentertainment.com>
2023-07-08 05:21:29 +0300

    on solar panel if load more then solar generates

---
+++ main.js
@@ -0,0 +1,28 @@
+const ewelink = require('ewelink-api');
+
+(async () => {
+
+    const connection = new ewelink({
+        email: 'maxpavlovdpvideo@gmail.com',
+        password: 'xxx',
+        region: 'eu',
+    });
+
+    /* get all devices */
+    // const devices = await connection.getDevices();
+    // console.log(connection)
+    // console.log(devices);
+
+    const p3 = await connection.getDevice("100169010c")
+    const solar = await connection.getDevice("10017c642b")
+
+    if (p3.params.power > 500) {
+        const status = await connection.setDevicePowerState('10017c642b', 'on');
+        console.log(status)
+    }
+
+    if (p3.params.power < 500) {
+        const status = await connection.setDevicePowerState('10017c642b', 'off');
+        console.log(status)
+    }
+})();
