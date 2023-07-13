3cc76f11d485fe0b9b0cfc68342c770c50c978e0
max <mpavlov@mrcentertainment.com>
2023-07-13 11:28:15 +0300

    risk

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ async function toggleGeneration(connection, state) {
     console.log("isDaytime: " + isDaytime)
 
     if (isDaytime &&
-        (counterPower < 70 || counterPower > GENERATION_MAX_POWER / OVER_GENERATION_RISK)
+        (counterPower < 110 || counterPower > GENERATION_MAX_POWER / OVER_GENERATION_RISK)
     ) {
         await toggleGeneration(connection, "on")
     } else {
